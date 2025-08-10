1b8300539c1187127f347e79ddc2f442de4c1443
leavethetrain <musik201092@web.de>
2025-08-10 16:49:12 +0200

    add dynamic background

--- src/mainMenu.js
+++ src/mainMenu.js
@@ -1,17 +1,19 @@
 import { rootElement } from "./main";
 import { renderLoadingScreen } from "./loadingScreen";
 import { loadDetailView } from "./detailView";
+import { getForecastWeather } from "./api";
+import { getConditionImagePath } from "./condition";
 
-export function loadCities() {
+export async function loadCities() {
   rootElement.classList.remove("show-background");
   renderLoadingScreen("Lade Übersicht...");
-  renderMainMenu();
+  await renderMainMenu();
 }
 
-function renderMainMenu() {
+async function renderMainMenu() {
   rootElement.innerHTML = `
   <div class="main-menu">${getMenuHeaderHtml()}
-  ${getCities()}</div>
+  ${await getCities()}</div>
   `;
 
   eventListeners();
@@ -36,20 +38,42 @@ function getMenuHeaderHtml() {
  `;
 }
 
-function getCities() {
+async function getCities() {
   const favoriteCities = ["Mannheim", "Muenchen", "Peking"];
   const favoriteCityEl = [];
 
   for (let city of favoriteCities) {
-    const cityHtml = `<div class="main-menu__citys" data-city-name="${city}">
+    const weatherData = await getForecastWeather(city, 1);
+
+    const { location, current, forecast } = weatherData;
+    const currentDay = forecast.forecastday[0];
+
+    const conditionImage = getConditionImagePath(
+      weatherData.current.condition.code,
+      weatherData.current.is_day !== 1
+    );
+
+    const cityHtml = `<div class="main-menu__citys" data-city-name="${city}" ${
+      conditionImage ? `style="--condition-image: url(${conditionImage})"` : ""
+    }>
                         <div class="main-menu__cityleft">
-                            <h2 class="main-menu__cityleft--location">${city}</h2>
-                            <p class="main-menu__cityleft--country">germany</p>
-                            <p class="main-menu__cityleft--condition">sonnig</p>
+                            <h2 class="main-menu__cityleft--location">${
+                              location.name
+                            }</h2>
+                            <p class="main-menu__cityleft--country">${
+                              location.country
+                            }</p>
+                            <p class="main-menu__cityleft--condition">${
+                              current.condition.text
+                            }</p>
                          </div>
                          <div class="main-menu__cityright">
-                            <p class="main-menu__cityright--currenttemp">88°</p>
-                            <p class="main-menu__cityright--maxmintemp">H:22° T:22°</p>
+                            <p class="main-menu__cityright--currenttemp">${Math.round(
+                              current.temp_c
+                            )}°</p>
+                            <p class="main-menu__cityright--maxmintemp">H:${Math.round(
+                              currentDay.day.maxtemp_c
+                            )}° T:${Math.round(currentDay.day.mintemp_c)}°</p>
                          </div>
                       </div>`;
 
